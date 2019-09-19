b34e5c7867973962c89df3aa3d0a82272ed39f79
Thinkman Wang <wangxf1985@gmail.com>
2019-09-19 19:46:14 +0800

    update

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from api import views
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     url(r'^hello$', views.hello, name='hello'),
     url(r'^fxxk$', views.ApiFxxk.as_view()),  # an example resource endpoint
-    url(r'^fxxk-plus/*', views.ApiFxxkPlus.as_view()),  # an example resource endpoint
+    url(r'^fxxk-plus/*', csrf_exempt(views.ApiFxxkPlus.as_view())),  # an example resource endpoint
 ]
--- api/views.py
+++ api/views.py
@@ -15,6 +15,8 @@ from rest_framework import generics
 from rest_framework.permissions import IsAdminUser, AllowAny
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope, OAuth2Authentication
 from rest_framework.authentication import SessionAuthentication
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 def hello(request):
     return HttpResponse(obj2json(RetModel()))
@@ -23,9 +25,11 @@ class ApiFxxk(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
         return HttpResponse(obj2json(RetModel(data="Hello, OAuth2!")))
 
+# @method_decorator(csrf_exempt)
 class ApiFxxkPlus(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
         return HttpResponse(obj2json(RetModel(data="GET FXXK, OAuth2!")))
 
+    # @method_decorator(csrf_exempt)
     def post(self, request, *args, **kwargs):
         return HttpResponse(obj2json(RetModel(data="POST FXXK, OAuth2!")))
