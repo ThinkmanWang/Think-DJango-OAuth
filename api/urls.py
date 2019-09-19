3689d85835c47ee007343950f93a987a2a035d92
Thinkman Wang <wangxf1985@gmail.com>
2019-09-19 12:00:11 +0800

    update

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from api import views
 
 urlpatterns = [
     url(r'^hello$', views.hello, name='hello'),
+    url(r'^fxxk$', views.ApiFxxk.as_view()),  # an example resource endpoint
+    url(r'^fxxk-plus/*', views.ApiFxxkPlus.as_view()),  # an example resource endpoint
 ]
--- api/views.py
+++ api/views.py
@@ -6,11 +6,26 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from pythinkutils.common.object2json import *
 from pythinkutils.common.RetModel import RetModel
+from oauth2_provider.views.generic import ProtectedResourceView
+from django.http import HttpResponse
 
-from rest_framework import generics, permissions, serializers
-from django.contrib.auth.decorators import login_required
+from django.urls import URLPattern, URLResolver
 
-from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope, authentication
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser, AllowAny
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope, OAuth2Authentication
+from rest_framework.authentication import SessionAuthentication
 
 def hello(request):
     return HttpResponse(obj2json(RetModel()))
+
+class ApiFxxk(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(obj2json(RetModel(data="Hello, OAuth2!")))
+
+class ApiFxxkPlus(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(obj2json(RetModel(data="GET FXXK, OAuth2!")))
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse(obj2json(RetModel(data="POST FXXK, OAuth2!")))
